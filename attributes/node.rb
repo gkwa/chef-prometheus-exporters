2bc63a41a48336846fa1d602975bb0ad8dc80dbd
Taylor Monacelli <taylormonacelli@gmail.com>
2017-09-22 20:56:11 -0700

    Get today's latest release node exporter

--- attributes/node.rb
+++ attributes/node.rb
@@ -1,4 +1,4 @@
-default['prometheus_exporters']['node']['version'] = '0.13.0'
+default['prometheus_exporters']['node']['version'] = '0.14.0'
 default['prometheus_exporters']['node']['url'] = "https://github.com/prometheus/node_exporter/releases/download/v#{node['prometheus_exporters']['node']['version']}/node_exporter-#{node['prometheus_exporters']['node']['version']}.linux-amd64.tar.gz"
 default['prometheus_exporters']['node']['checksum'] = '2de5d1e51330c41588ed4c88bc531a3d2dccf6b4d7b99d5782d95cff27a3c049'
 
